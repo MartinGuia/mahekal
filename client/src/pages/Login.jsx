53ed09a25cb0ab32db489d2c35723aa9c7b60b20
armenta <armentaemanuel1107@gmail.com>
2024-01-30 13:02:13 -0600

    Color modification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
-import NavBar from "../components/NavBar";
-
 export default function Login() {
   return (
     <div className=" flex h-[calc(100vh-100px)] items-center p-5 justify-center">
-      <div className="bg-slate-100 max-w-lg w-full p-8 rounded text-center shadow-xl">
+      <div className="bg-white max-w-lg w-full p-8 rounded text-center shadow-xl">
         <h1 className="font-bold">MAHEKAL</h1>
         <form className="flex flex-col items-center">
           <img className="w-20 p-1" src="LogoMahekal.png" alt="Mahekal Logo" />
           <input
-            className="w-full p-2 rounded m-2"
+            className="w-full bg-mahekal-input p-2 rounded m-2"
             type="text"
             placeholder="User"
           />
           <input
-            className="w-full p-2 rounded m-2"
+            className="w-full bg-mahekal-input p-2 rounded m-2"
             type="password"
             placeholder="Password"
           />
-          <button className="p-2 m-2 w-1/2 rounded-lg bg-cyan-300 hover:bg-cyan-400" type="submit">
+          <button className="p-2 m-2 w-1/2 rounded-lg bg-water-blue hover:bg-water-blue-hover" type="submit">
             Login
           </button>
         </form>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
     extend: {
       colors: {
         "water-blue" : "#2CCCD3",
+        "water-blue-hover" : "#31b5ba",
         "light-bone": "#F6F5F0",
         "mahekal-brown": "#614F25",
+        "mahekal-input": "#e2e8f0"
       }
     },
   },
-  
+  plugins: [],
 }
 
