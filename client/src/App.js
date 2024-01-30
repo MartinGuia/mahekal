2b6edfadc3c8d3c3eff485124179b9fa3b90274c
Martin Garcia Guia <martin_sillo7@hotmail.com>
2024-01-30 12:12:39 -0500

    SideBarComponent creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TicketsPage from './pages/TicketsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>DASHBOARD</h1>}/>
         <Route path='/login' element={<h1>LOGIN</h1>}/>
+        <Route path='/tickets' element={<TicketsPage/>}/>
         <Route path='/register' element={<h1>REGISTER</h1>}/>
         <Route path='/ticket/:id' element={<h1>TICKET</h1>}/>
         <Route path='/profile' element={<h1>PROFILE</h1>}/>
