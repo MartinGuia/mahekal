26b4c213cb89b3d7c72912001f601b9b8ce67f4c
Martin Garcia Guia <martin_sillo7@hotmail.com>
2024-02-17 11:02:08 -0500

    updating components and creating missing views

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import NewTicketPage from './pages/NewTicketPage';
 import Login from "./pages/Login";
 import DepartamentosPage from './pages/DepartamentosPage';
 import ListOfDeptCollabs from './pages/ListOfDeptCollabs';
+import AllAccountPage from './pages/AllAccountPage';
+import AccountCollabPage from './pages/AccountCollabPage';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/newticket' element={<NewTicketPage/>}/>
         <Route path='/departamentos' element={<DepartamentosPage/>}/>
         <Route path='/listadptocollabs' element={<ListOfDeptCollabs/>}/>
+        <Route path='/allaccounts' element={<AllAccountPage/>}/>
+        <Route path='/account' element={<AccountCollabPage/>}/>
         <Route path='/register' element={<h1>REGISTER</h1>}/>
         <Route path='/ticket/:id' element={<h1>TICKET</h1>}/>
         <Route path='/profile' element={<h1>PROFILE</h1>}/>
---
+++ client/src/pages/AccountCollabPage.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import { Title } from '../components/Headers/Title'
+import { Link } from 'react-router-dom'
+import { Tarjeta } from '../components/ui/Tarjeta'
+
+function AccountCollabPage() {
+  return (
+    <Nav>
+        <Title>Cuenta</Title>
+        {/* Caja que contiene el boton para regresar una pagina atras */}
+        <div className="w-[9%] bottom-9 left-6 relative">
+          <button className="rounded-full shadow-md">
+            <Link to="/allaccounts">
+              <img
+                src="flechaAtras.png"
+                className="size-8 max-[281px]:size-6"
+                alt=""
+              />
+            </Link>
+          </button>
+        </div>  
+
+        <section className='w-[100%] h-auto'>
+            <div className='w-[100%] flex justify-center'>
+            <Tarjeta className='w-[80%] h-auto flex-col items-center max-[1281px]:w-[90%] max-[391px]:w-[90%]'>
+                <div className='w-[100%] h-36 flex justify-center items-center'>
+                    <div className='w-[90%] flex'>
+                        <div className=''>
+                            <Title className=''>Martin García</Title>
+                        </div>
+                    </div>
+                    {/* <div className='w-[50%]'>
+                        <div className='w-[100%] flex justify-center'>
+                            <button>
+                                <p>Mensaje</p>
+                                <img src="" alt="" />
+                            </button>
+                        </div>
+                    </div> */}
+                </div>
+                <div className='w-[100%] flex h-20 bg-slate-300 rounded-md items-center'>
+                    <div className='mx-4 text-xl'>Tickets</div>
+                    <div className='mx-4 text-xl'>Notas</div>
+                    <div className='mx-4 text-xl'>Estadísticas</div>
+                </div>
+            </Tarjeta>
+        </div>
+        </section>
+    </Nav>
+  )
+}
+
+export default AccountCollabPage
