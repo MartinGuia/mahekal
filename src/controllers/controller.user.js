4f87f3c07cbe01efbd0ba2e5ac8ddd6c8d773d1d
Martin Garcia Guia <martin_sillo7@hotmail.com>
2024-02-22 14:58:59 -0500

    update 3.0

--- src/controllers/controller.user.js
+++
@@ -1,3 +0,0 @@
-export const getUser = (req, res, next) => {
-    res.json("Endpoint user successfully")
-}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,10 +6,14 @@ import verifyRoleAdmin from "../middlewares/verifyRoleAdmin.js";
 const router = Router();
 
 router.post("/signin", authController.signin);
+router.post("/signin", authController.signin);
 
 router.post("/signup",  authController.signup);
 router.get("/signup", verifyToken, verifyRoleAdmin, authController.getSignup);
 router.get("/verify", verifyToken)
 router.post("/logout", verifyToken, authController.logout);
 
-export default router;
+router.post('/logout', verifyToken, authController.logout)
+
+
+export default router
