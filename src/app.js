8e5f9cb5a5a542660f872c0b4da68f12b5b56370
armenta <armentaemanuel1107@gmail.com>
2024-03-12 12:31:43 -0600

    Get departmets endpoint

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import morgan from "morgan";
-import authRouthes from "./routes/auth.routes.js";
-import userRouthes from "./routes/accounts.routes.js";
-import ticketsRouthes from "./routes/tickets.routes.js";
-import departamentsRouthes from "./routes/departament.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/accounts.routes.js";
+import ticketsRoutes from "./routes/tickets.routes.js";
+import departamentsRoutes from "./routes/departament.routes.js";
 import { createRoles } from "./libs/initialRoles.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 import { checkTicketExpired } from "./middlewares/checkTicketExpired.js";
+import rolesRoutes from "./routes/roles.routes.js";
 
 const app = express();
 createRoles();
@@ -23,9 +24,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRouthes);
-app.use("/api/users", userRouthes);
-app.use("/api/tickets", ticketsRouthes);
-app.use("/api/departments", departamentsRouthes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tickets", ticketsRoutes);
+app.use("/api/departments", departamentsRoutes);
+app.use("/api/roles", rolesRoutes);
 
 export default app;
