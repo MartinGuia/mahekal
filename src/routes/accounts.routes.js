dd9bfa4d3ca2a7509996bb5d2784e6ceaaaa443b
armenta <armentaemanuel1107@gmail.com>
2024-03-19 09:17:43 -0600

    Fixes to modify collaborator

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -5,8 +5,10 @@ const router = Router();
 
 router.get("/get-allUsers", accountsController.getAllUsers);
 router.get("/getUserToModify/:id", accountsController.getUserByIdToModify);
+
+// Ruta para traer los tickets
 router.get("/getUser/:id", accountsController.getUserById);
-router.put("/update-user", accountsController.updateUser);
-router.put("/update-password", accountsController.updatePassword);
+router.put("/update-user/:id", accountsController.updateUser);
+router.put("/update-password/:id", accountsController.updatePassword);
 
 export default router;
